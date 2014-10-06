d5692d898cdb0b3e4f37b228cae41c46d373b48f
koutnij <koutnij@x61s.idsia.ch>
2014-10-06 12:03:00 +0200

    added fork=yes to vm-run and target

--- vm-run.sh
+++ vm-run.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+
+# This script is just a testing one, it does the same as running ant vm-run
+
 java -cp lib/libthrift-1.0.0.jar:lib/log4j-1.2-1.2.16.jar:lib/slf4j-log4j12-1.6.5.jar:lib/slf4j-api-1.6.5.jar:dist/nascence-api.jar:dist/virtual-material.jar nascence.vm.thrift.VirtualMaterialServer
